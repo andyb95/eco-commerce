91ac2b960f143bd00fe845f8a44ea0ef25de5c04
andyb95 <andybeutler95@gmail.com>
2021-01-01 19:21:11 -0700

    converted to function component

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,71 +1,63 @@
 //state: search
 //methods: handleChange, toggleSearch
-import React, {Component} from 'react'
+import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
-
 import {updateUser, logout} from './../../redux/userReducer'
 import logo from '../../screenshots/eco-shop.png'
 import './Nav.css'
 
+const Nav = (props) => {
+  const {name, email} = props.userReducer
+  return(
+    <div className= 'nav'>
 
-class Nav extends Component{
-  
-
-  render(){
-    const {name} = this.props.userReducer
-    const {email} = this.props.userReducer
-    return(
-      <div className= 'nav'>
-
-        <Link to = '/'>
-            <img src = {logo}
-            className= 'home-button'
-            alt='logo'
-            />
-        </Link>
-
-        <div className = 'search-bar'>
-         <input
-            className = 'input'
-            type = 'search'
+      <Link to = '/'>
+          <img src = {logo}
+          className= 'home-button'
+          alt='logo'
           />
-          <button className= 'search-button' >
-            <img className= 'search-icon' src ='https://www.pngmart.com/files/8/Search-Button-PNG-Image-Free-Download.png' alt='searchIcon'/>
-          </button>
-        </div>
-    
-        {name ? (
-          <div>
-            <Link className = 'hello' to = '/user'>Hello, {name}
-            <p className = 'link'>Account</p>
-            </Link>        
-          </div>
-          ): email ? (
-            <div>
-              <Link className = 'hello' to = '/user'>Hello, {email}
-              <p className = 'link'>Account</p>
-              </Link>
-            </div>
-            ):(
-            <Link className = 'hello' to = '/landing'>Hello, Sign In</Link>
-            )}
+      </Link>
 
-          <Link to = '/cart'>
-            <img 
-              className = 'cart-img' 
-              src = 'https://image.flaticon.com/icons/png/512/34/34627.png'
-              alt='cart'
-            /> 
-            </Link>
+      <div className = 'search-bar'>
+       <input
+          className = 'input'
+          type = 'search'
+        />
+        <button className= 'search-button' >
+          <img className= 'search-icon' src ='https://www.pngmart.com/files/8/Search-Button-PNG-Image-Free-Download.png' alt='searchIcon'/>
+        </button>
       </div>
-    )
-  }
+  
+      {name ? (
+        <div>
+          <Link className = 'hello' to = '/user'>Hello, {name}
+          <p className = 'link'>Account</p>
+          </Link>        
+        </div>
+        ): email ? (
+          <div>
+            <Link className = 'hello' to = '/user'>Hello, {email}
+            <p className = 'link'>Account</p>
+            </Link>
+          </div>
+          ):(
+          <Link className = 'hello' to = '/landing'>Hello, Sign In</Link>
+          )}
+
+        <Link to = '/cart'>
+          <img 
+            className = 'cart-img' 
+            src = 'https://image.flaticon.com/icons/png/512/34/34627.png'
+            alt='cart'
+          /> 
+          </Link>
+    </div>
+  )
 }
 
 function mapStateToProps(state){
   return state
 }
  
-
 export default connect(mapStateToProps, {updateUser, logout})(Nav)
