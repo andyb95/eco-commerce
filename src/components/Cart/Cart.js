43ecaaddd993bd9f01b5d4309415d0585054c943
andyb95 <andybeutler95@gmail.com>
2021-01-05 22:25:49 -0700

    cart count now updates when product is removed from cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,70 +1,63 @@
-import React, {Component} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import CartProduct from '../CartProduct/CartProduct'
-
 import './Cart.css'
 
 
-class Cart extends Component{
-  constructor(props){
-    super(props)
-    this.state={
-      cart: [],
-      total: []
+const Cart = ({ userReducer }) => {
+  const { user_id } = userReducer
+  const [cart, setCart] = useState([])
+  const [total, setTotal] = useState([])
+  const [cartRetrieved, setCartRetrieved] = useState(false)
+
+  useEffect(() => {
+    const getCart = () => {
+      axios.get(`/api/users/${user_id}/cart`)
+      .then(res => {
+        setCart(res.data)
+      })
+      .catch(err => console.log(err))
     }
-  }
+    
+    const getTotal = () => {
+      axios.get(`/api/users/${user_id}/total`)
+      .then(res => {
+        setTotal(res.data[0].sum)
+      })
+      .catch(err => console.log(err))
+    }
+    if (!cartRetrieved) {
+      getCart()
+      getTotal()
+      setCartRetrieved(true)
+    }
+  },[cartRetrieved, user_id])
 
-  componentDidMount(){
-    this.getCart()
-    this.getTotal()
-  }
-
-  getCart = () => {
-    const {user_id} = this.props.userReducer
-    axios.get(`/api/users/${user_id}/cart`)
-    .then(res => {
-      this.setState({cart: res.data})
-    })
-    .catch(err => console.log(err))
-  }
-  
-  getTotal = () => {
-    const {user_id} = this.props.userReducer
-    axios.get(`/api/users/${user_id}/total`)
-    .then(res => {
-      this.setState({total: res.data[0].sum})
-      console.log(this.state.total)
-    })
-    .catch(err => console.log(err))
-  }
-
-  render(){
-
-    return (
-      <div className = 'cart'>
-        {!this.state.cart[0] ? (
-          <h1>Sign in to fill your Cart and help save the Earth.</h1>
-        ):(
-          <div>
-            {this.state.cart.map((e) => {
-              return <CartProduct
-              key = {e.cart_id}
-              product = {e}
-              getCart = {this.getCart}
-              getTotal = {this.getTotal}
+  return (
+    <div className = 'cart'>
+      {cart[0] ? (
+        <div>
+          {cart.map((e) => {
+            return (
+              <CartProduct
+                key={e.cart_id}
+                product={e}
+                setCartRetrieved={setCartRetrieved}
               />
-            })}
-          <h1>Total: {this.state.total}</h1>
-          <button className = 'checkout-button'>
-            <Link className = 'home-link-link' to ='/checkout'>Checkout</Link>
-          </button>
-          </div>
-        )}
-      </div>
-    )
-  }
+            )
+          })}
+        <h1>Total: {total}</h1>
+        <button className = 'checkout-button'>
+          <Link className = 'home-link-link' to ='/checkout'>Checkout</Link>
+        </button>
+        </div>
+      ) : (
+        <h1>Sign in to fill your cart</h1>
+      )}
+    </div>
+  )
 }
 
 const mapStateToProps = state => {
